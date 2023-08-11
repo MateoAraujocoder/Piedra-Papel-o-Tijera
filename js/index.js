c226c122e323925c009189dc9310381e72d720cc
Mateo Araujo <oliaramateo@gmail.com>
2023-08-11 10:26:10 -0300

    agrego arrays

--- js/index.js
+++ js/index.js
@@ -1,48 +1,50 @@
-
 alert("¡Bienvenido jugador!");
+
+/*objetos*/
+const opciones = ["piedra", "papel", "tijera"];
+const mensajes = ["Elegiste", "El enemigo eligió", "Empate", "¡ HAS GANADO :) !", " Perdiste :( "];
 
 let continuarJugando = true;
 
 while (continuarJugando) {
-    alert("Elige tu ataque:");
 
-    let player = parseInt(prompt("Escribe 0 para piedra, 1 para papel y 2 para tijera"));
+    for (let i = 0; i < 3; i++) {
+        alert("Elige tu ataque:");
 
-    if (player === 0) {
-        alert("Elegiste piedra");
-    } else if (player === 1) {
-        alert("Elegiste papel");
-    } else if (player === 2) {
-        alert("Elegiste tijera");
-    } else {
-        alert("Selección inválida. Por favor, elige nuevamente.");
-        continue; // Regresa al inicio del ciclo para una nueva elección
-    }
+        let player = parseInt(prompt("Escribe 0 para piedra, 1 para papel y 2 para tijera"));
 
-    let enemy = Math.round(Math.random() * 2);
+        if (player >= 0 && player <= 2) {
+            alert(`${mensajes[0]} ${opciones[player]}`);
+        } else {
+            alert("Selección inválida. Por favor, elige nuevamente.");
+            i--; /* se le resta 1 al contador para que el jugador repita la elección*/
 
-    if (enemy === 0) {
-        alert("El enemigo eligió piedra");
-    } else if (enemy === 1) {
-        alert("El enemigo eligió papel");
-    } else if (enemy === 2) {
-        alert("El enemigo eligió tijera");
-    }
+            continue;
+        }
 
-    if (player === enemy) {
-        alert("Empate");
-    } else if ((player === 0 && enemy === 2) || (player === 1 && enemy === 0) || (player === 2 && enemy === 1)) {
-        alert("¡HAS GANADO!");
-    } else {
-        alert("Perdiste :(");
+        let enemy = Math.floor(Math.random() * 3);
+        alert(`${mensajes[1]} ${opciones[enemy]}`);
+
+        if (player === enemy) {
+            alert(mensajes[2]);
+
+        } else if ((player === 0 && enemy === 2) || (player === 1 && enemy === 0) || (player === 2 && enemy === 1)) {
+            alert(mensajes[3]);
+
+        } else {
+            alert(mensajes[4]);
+        }
     }
 
     let decision = prompt("¿Quieres jugar de nuevo? (Sí/No)").toLowerCase();
-    if (decision == "sí") {
+    if (decision == "si") {
+
         continuarJugando = true;
 
-    }else if (decision!=="si"){
+    } else { (decision!== "si")
+
         continuarJugando = false;
+  
     }
 }
 

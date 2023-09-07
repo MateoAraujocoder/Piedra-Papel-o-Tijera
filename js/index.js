4abbf444897c0fd1d5dfe6acafdcab1040578534
Mateo Araujo <oliaramateo@gmail.com>
2023-09-07 11:33:59 -0300

    elimino alert y prompt

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const iniciarJuegoBtn = document.getElementById("iniciarJuego");
-    iniciarJuegoBtn.addEventListener("click", function () {
-        
-        alert("¡Bienvenido jugador!")
+    const mensajesContainer = document.getElementById("mensajesContainer");
+    const resultadosContainer = document.getElementById("resultadosContainer");
+    const reiniciarBtn = document.getElementById("reiniciarBtn");
 
-        /* Constructores */
+    iniciarJuegoBtn.addEventListener("click", function () {
+        mensajesContainer.innerHTML = "¡Bienvenido jugador!<br>";
+
         class Jugador {
             constructor(nombre) {
                 this.nombre = nombre;
@@ -32,16 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
 
-        const alertMessage = message => alert(message);
-        const promptInput = message => parseInt(prompt(message));
-
-        /* Objetos */
         const opciones = ["piedra", "papel", "tijera"];
         const mensajes = ["Elegiste", "El enemigo eligió", "Empate ._.", "¡HAS GANADO! :)", "Perdiste :("];
+        let rondasJugadas = 0;
 
-        let continuarJugando = true;
-
-        /* Personas enemigas */
         const enemy = [
             new Enemigo("Ana"),
             new Enemigo("Facundo"),
@@ -57,89 +52,152 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const mostrarNombresEnemigos = () => {
             const nombresEnemigos = enemy.map(enemigo => enemigo.nombre).join(', ');
-            alert(`Nombres de enemigos disponibles: ${nombresEnemigos}`);
+            const nombresEnemigosMensaje = document.createElement("p");
+            nombresEnemigosMensaje.textContent = `Nombres de enemigos disponibles: ${nombresEnemigos}`;
+            mensajesContainer.appendChild(nombresEnemigosMensaje);
         };
 
         const obtenerNombreEnemigo = () => {
             mostrarNombresEnemigos();
-            let nombreEnemigoElegido = prompt("Elige el nombre de un enemigo para enfrentar:").trim();
-            while (!enemy.some(enemigo => enemigo.nombre.toLowerCase() === nombreEnemigoElegido.toLowerCase())) {
-                alert(`No se encontró ningún enemigo con el nombre ${nombreEnemigoElegido}.`);
-                nombreEnemigoElegido = prompt("Por favor, elige un nombre válido de enemigo:").trim();
-            }
-            return nombreEnemigoElegido;
+
+            const nombreEnemigoInput = document.createElement("input");
+            nombreEnemigoInput.type = "text";
+            nombreEnemigoInput.placeholder = "Elige el nombre de un enemigo para enfrentar:";
+            mensajesContainer.appendChild(nombreEnemigoInput);
+
+            return new Promise(resolve => {
+                nombreEnemigoInput.addEventListener("change", function () {
+                    let nombreEnemigoElegido = nombreEnemigoInput.value.trim();
+                    while (!enemy.some(enemigo => enemigo.nombre.toLowerCase() === nombreEnemigoElegido.toLowerCase())) {
+                        const noEncontradoMensaje = document.createElement("p");
+                        noEncontradoMensaje.textContent = `No se encontró ningún enemigo con el nombre ${nombreEnemigoElegido}.`;
+                        mensajesContainer.appendChild(noEncontradoMensaje);
+                        nombreEnemigoElegido = nombreEnemigoInput.value.trim();
+                    }
+                    resolve(nombreEnemigoElegido);
+                });
+            });
         };
 
-        /* Inicio del juego */
-        const obtenerEntrada = mensaje => promptInput(mensaje);
+        const obtenerEntrada = mensaje => {
+            return new Promise(resolve => {
+                const input = document.createElement("input");
+                input.type = "number";
+                input.placeholder = mensaje;
+                mensajesContainer.appendChild(input);
 
-        const mostrarResultado = (mensaje, jugadorGana) => {
-            alert(mensaje);
-            if (jugadorGana) {
-                jugador.incrementarPartidasGanadas(); /* Usando el método personalizado*/
-            }
+                input.addEventListener("change", function () {
+                    resolve(parseInt(input.value));
+                });
+            });
         };
 
-        const jugadorNombre = prompt("Ingresa tu nombre:");
-        const jugador = new Jugador(jugadorNombre);
+        const mostrarResultado = (jugadorGana) => {
+            const resultadoMensaje = document.createElement("p");
+            resultadoMensaje.textContent = mensajes[jugadorGana ? 3 : 4];
+            mensajesContainer.appendChild(resultadoMensaje);
+        };
 
-        // Verifica si hay datos previamente guardados en localStorage
-        const jugadorGuardado = localStorage.getItem('jugador');
-        const partidasGanadasGuardadas = localStorage.getItem('partidasGanadas');
+        const jugadorNombreInput = document.createElement("input");
+        jugadorNombreInput.type = "text";
+        jugadorNombreInput.placeholder = "Ingresa tu nombre:";
+        mensajesContainer.appendChild(jugadorNombreInput);
 
-        if (jugadorGuardado && partidasGanadasGuardadas) {
-            jugador.nombre = jugadorGuardado;
-            jugador.partidasGanadas = parseInt(partidasGanadasGuardadas);
-        }
+        jugadorNombreInput.addEventListener("change", async function () {
+            const jugadorNombre = jugadorNombreInput.value;
 
-        const jugar = () => {
-            while (continuarJugando) {
-                for (let i = 0; i < 3; i++) {
-                    alert("Elige tu ataque:");
+            // Obtener todos los nombres y progresos almacenados en el localStorage
+            const nombresGuardados = JSON.parse(localStorage.getItem('nombres')) || [];
+            const partidasGanadasGuardadas = localStorage.getItem('partidasGanadas');
 
-                    let player = obtenerEntrada("Escribe 0 para piedra, 1 para papel y 2 para tijera");
+            let jugador;
+
+            // Buscar si el nombre ya está en la lista de nombres guardados
+            const jugadorGuardado = nombresGuardados.find(entry => entry.nombre === jugadorNombre);
+
+            if (jugadorGuardado) {
+                // Si el nombre ya existe, usar el progreso almacenado
+                jugador = new Jugador(jugadorGuardado.nombre);
+                jugador.partidasGanadas = parseInt(jugadorGuardado.partidasGanadas);
+            } else {
+                // Si es un nuevo nombre, crear un nuevo jugador
+                jugador = new Jugador(jugadorNombre);
+                nombresGuardados.push({ nombre: jugador.nombre, partidasGanadas: jugador.partidasGanadas });
+                localStorage.setItem('nombres', JSON.stringify(nombresGuardados));
+            }
+
+            const jugar = async () => {
+                while (rondasJugadas < 3) {
+                    rondasJugadas++;
+
+                    const ataqueMensaje = document.createElement("p");
+                    ataqueMensaje.textContent = "Ronda " + rondasJugadas + ": Elige tu ataque:";
+                    mensajesContainer.appendChild(ataqueMensaje);
+
+                    const player = await obtenerEntrada("Escribe 0 para piedra, 1 para papel y 2 para tijera:");
 
                     if (player >= 0 && player <= 2) {
-                        alert(`${mensajes[0]} ${opciones[player]}`);
+                        const eleccionMensaje = document.createElement("p");
+                        eleccionMensaje.textContent = `${mensajes[0]} ${opciones[player]}`;
+                        mensajesContainer.appendChild(eleccionMensaje);
+
+                        const nombreEnemigoElegido = await obtenerNombreEnemigo();
+                        const enemigo = Enemigo.buscarEnemigoPorNombre(nombreEnemigoElegido);
+
+                        const enemigoAtaque = enemigo.ataque;
+                        const enemigoAtaqueMensaje = document.createElement("p");
+                        enemigoAtaqueMensaje.textContent = `${enemigo.nombre} eligió ${opciones[enemigoAtaque]}`;
+                        mensajesContainer.appendChild(enemigoAtaqueMensaje);
+
+                        const resultado = determinarResultado(player, enemigoAtaque);
+
+                        // Muestra el mensaje de resultado debajo del mensaje del ataque del enemigo
+                        const resultadoMensaje = document.createElement("p");
+                        resultadoMensaje.textContent = mensajes[resultado];
+                        mensajesContainer.appendChild(resultadoMensaje);
+
+                        mostrarResultado(resultado === 3);
+
+                        // Pausa para mostrar el resultado antes de la siguiente ronda
+                        await new Promise(resolve => setTimeout(resolve, 1000));
                     } else {
-                        alert("Selección inválida. Por favor, elige nuevamente.");
-                        i--;
-
-                        continue;
-                    }
-
-                    let nombreEnemigoElegido = obtenerNombreEnemigo();
-                    let enemigoElegido = Enemigo.buscarEnemigoPorNombre(nombreEnemigoElegido);
-
-                    alert(` ${enemigoElegido.nombre} eligió ${enemigoElegido.mostrarAtaque()}`);
-
-                    if (player === enemigoElegido.ataque) {
-                        alert(mensajes[2]);
-                    } else if ((player === 0 && enemigoElegido.ataque === 2) || (player === 1 && enemigoElegido.ataque === 0) || (player === 2 && enemigoElegido.ataque === 1)) {
-                        mostrarResultado(mensajes[3], true);
-                    } else {
-                        mostrarResultado(mensajes[4], false);
+                        const seleccionInvalidaMensaje = document.createElement("p");
+                        seleccionInvalidaMensaje.textContent = "Selección inválida. Por favor, elige nuevamente.";
+                        mensajesContainer.appendChild(seleccionInvalidaMensaje);
+                        rondasJugadas--; // Resta una ronda para que se repita la actual.
                     }
                 }
 
-                alert(`Has ganado ${jugador.partidasGanadas} partidas.`);
+                const partidasGanadasMensaje = document.createElement("p");
+                partidasGanadasMensaje.textContent = `Has ganado ${jugador.partidasGanadas} partidas.`;
+                resultadosContainer.appendChild(partidasGanadasMensaje);
 
-                let decision = prompt("¿Quieres jugar de nuevo? (Sí/No)").toLowerCase();
-
-                if (decision.indexOf("no") !== -1) {
-                    continuarJugando = false;
-                }
-
-                // Guarda los datos actualizados del jugador en localStorage
                 localStorage.setItem('jugador', jugador.nombre);
                 localStorage.setItem('partidasGanadas', jugador.partidasGanadas.toString());
+
+                // Mostrar el botón para reiniciar
+                reiniciarBtn.style.display = "block";
+            };
+
+            jugar();
+        });
+
+        reiniciarBtn.addEventListener("click", function () {
+            // Limpia los mensajes y resultados para reiniciar el juego
+            mensajesContainer.innerHTML = "";
+            resultadosContainer.innerHTML = "";
+            rondasJugadas = 0;
+            reiniciarBtn.style.display = "none";
+        });
+
+        function determinarResultado(jugador, enemigo) {
+            if (jugador === enemigo) {
+                return 2; // Empate
+            } else if ((jugador === 0 && enemigo === 2) || (jugador === 1 && enemigo === 0) || (jugador === 2 && enemigo === 1)) {
+                return 3; // Jugador gana
+            } else {
+                return 4; // Jugador pierde
             }
-
-            alert("¡Gracias por jugar!");
-        };
-
-        // Llamada a la función para iniciar el juego
-        jugar();
+        }
     });
 });
-
